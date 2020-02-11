9c8756c9b98429d52d9e1d946c5977cd4495686a
xmmwc <mumu@mgtv.com>
2020-02-11 10:57:05 +0800

    :art: use local tr in 24 hours

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const fse = require('fs-extra')
 const path = require('path')
+const fetch = require('node-fetch')
 const program = require('commander')
-const axios = require('axios')
 const version = require('./package.json').version
 
 const trPath = path.join(__dirname, 'tr.txt')
@@ -14,34 +14,47 @@ const collect = (val, memo) => {
 }
 
 const getList = () => {
-  return axios.get('https://github.com/ngosang/trackerslist/raw/master/trackers_best.txt').then(data => {
-    saveTrackers(data.data)
-    const list = data.data.trim().split('\n\n')
+  return fetch("https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt", {"credentials":"omit","headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","accept-language":"zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,zh-TW;q=0.6","cache-control":"no-cache","pragma":"no-cache","sec-fetch-mode":"navigate","sec-fetch-site":"none","sec-fetch-user":"?1","upgrade-insecure-requests":"1"},"referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"}).then(res => {
+    return res.text()
+  }).then(data => {
+    saveTrackers(data)
+    const list = data.trim().split('\n\n')
     return list
   }).catch(e => {
     console.error(e.message)
     console.warn('获取tr失败，使用本地tr')
-    return getLocalTrackers()
+    return getLocalTrackers().then(({ list }) => list)
   })
 }
 
 const saveTrackers = data => {
-  fse.writeFileSync(trPath, data)
+  const now = new Date().getTime()
+  const dataWithTime = `${data}-$${now}`
+  fse.writeFileSync(trPath, dataWithTime)
   console.log('保存tr成功')
 }
 
 const getLocalTrackers = () => {
   if (fse.pathExistsSync(trPath)) {
     const buf = fse.readFileSync(trPath)
-    const data = buf.toString()
+    const dataWithDate = buf.toString()
+    const [data, time] = dataWithDate.split('-$')
     const list = data.trim().split('\n\n')
-    return list
+    return Promise.resolve({ list, time })
   }
-  return []
+  return Promise.resolve({ list, time })
 }
 
 const parse = () => {
-  getList().then((list) => {
+  getLocalTrackers().then(({ list, time }) => {
+    const now = new Date().getTime()
+    const oneDay = 24 * 60 * 60 * 1000
+    if (!time || now > (+time) + oneDay) {
+      return getList()
+    } else {
+      return list
+    }
+  }).then((list) => {
     let originMagnet = ''
     program
       .version(version)
